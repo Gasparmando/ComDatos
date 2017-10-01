7c38a40ec7866417bc0977f533daeff0388613a0
Gaspar <gasparmando@hotmail.com>
2017-10-01 20:33:40 -0300

    arreglo server

--- TP3/serverTCP.py
+++ TP3/serverTCP.py
@@ -3,20 +3,20 @@ import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('locahost', 1998)
+server_address = ('192.168.1.6', 1998)
 sock.bind(server_address)
 
 sock.listen(1)
 
 while True:
-	print >>sys.strerr, 'Esperando una conexion ...'
+	print >>sys.stderr, 'Esperando una conexion ...'
 	connection, client_address = sock.accept()
 	
 	try:
 		print >>sys.stderr, 'Conexion de', client_address
 		while True:
 			data = connection.recv(16)
-			print >>sys.strerr, 'Recibido "%s"' % data
+			print >>sys.stderr, 'Recibido "%s"' % data
 			if data:
 				print >>sys.stderr, 'Reenviando data al cliente'
 				connection.sendall(data)
